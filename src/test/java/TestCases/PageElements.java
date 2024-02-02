4f87d9827881f30a31e07ce107b2218fa80c5867
shaym <shaymaa2012@gmail.com>
2024-02-02 18:08:54 -0500

    Page Elements

---
+++ src/test/java/TestCases/PageElements.java
@@ -0,0 +1,36 @@
+package TestCases;
+
+import Utilities.DriverClass;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class PageElements {
+
+    public PageElements(){
+        PageFactory.initElements(DriverClass.driver,this);
+    }
+
+    @FindBy(css = "a[class=\"zak-button\"]")
+    public WebElement demo;
+
+   @FindBy(xpath = "//span[text()=\"Enter the OpenMRS 2 Demo\"]")
+    public WebElement enterTheOpenMRS;
+
+   @FindBy(css = "input[placeholder=\"Enter your username\"]")
+    public WebElement enterUserName;
+
+   @FindBy(css = "i[id=\"togglePassword\"]")
+   public  WebElement revealPasswordButton;
+
+   @FindBy (css = "input[placeholder=\"Enter your password\"]")
+    public WebElement enterPassWord;
+
+   @FindBy (css = "li[id=\"Inpatient Ward\"]")
+    public WebElement inpatientWard;
+
+   @FindBy (css = "input[id=\"loginButton\"]")
+    public WebElement logInButton;
+
+
+}
